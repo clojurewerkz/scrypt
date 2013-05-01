d7894f16a873fb976f657caa2276fe2277a19096
Michael Klishin <michael@defprotocol.org>
2013-05-01 13:28:40 +0400

    1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/scrypt "1.0.0-SNAPSHOT"
+(defproject clojurewerkz/scrypt "1.0.0"
   :description "A Clojure library for scrypt encryption"
   :dependencies [[org.clojure/clojure    "1.5.1"]
                  [com.lambdaworks/scrypt "1.3.3"]]
