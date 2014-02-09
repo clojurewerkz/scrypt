4186ee53465f6aa0aa49fb36f0b5c4c072ad8bc6
Michael Klishin <michael@clojurewerkz.org>
2014-02-09 16:50:22 +0400

    License header

--- src/clojure/clojurewerkz/scrypt/core.clj
+++ src/clojure/clojurewerkz/scrypt/core.clj
@@ -1,3 +1,5 @@
+;; Copyright (c) 2013-2014 Michael S. Klishin, Alex Petrov, and the ClojureWerkz Team
+;;
 ;; This file is provided to you under the Apache License, Version 2.0 (the
 ;; "License"); you may not use this file except in compliance with the License.
 ;; You may obtain a copy of the License at
